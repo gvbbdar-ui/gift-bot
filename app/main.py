1526cc2b741e72390253aa50a042561290280f75
gvbbdar-ui <gvbbdar@gmail.com>
2026-07-15 08:43:47 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
+from loguru import logger
 
 from app.config import settings
 from app.handlers.start import router as start_router
+from app.handlers.evaluate import router as evaluate_router
 
 async def main():
     bot = Bot(token=settings.BOT_TOKEN)
     dp = Dispatcher()
 
     dp.include_router(start_router)
+    dp.include_router(evaluate_router)
 
+    logger.info("🚀 Бот запущен")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
